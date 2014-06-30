b5bb0e4ae4f1b2a19df91fd3925b6cd54ad2dac1
Tyler Gillies <tjgillies@gmail.com>
2014-06-29 22:37:11 -0700

    no message

--- umt/AppDelegate.m
+++ umt/AppDelegate.m
@@ -8,11 +8,61 @@
 
 #import "AppDelegate.h"
 
-@implementation AppDelegate
+NSString * umt(){
+    NSDate *today = [NSDate date];
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *weekdayComponents =
+    [gregorian components:(NSHourCalendarUnit | NSMinuteCalendarUnit|NSSecondCalendarUnit) fromDate:today];
+    NSInteger minute = [weekdayComponents minute];
+    NSInteger hour = [weekdayComponents hour];
+    NSInteger second = [weekdayComponents second];
+    float offset = [[NSTimeZone localTimeZone] secondsFromGMT] /60;
+    NSInteger idl = ((60 * hour) + (abs(offset) - 720));
+    if (idl < 0) idl = 1440 + idl;
+        idl = idl / 60;
+        NSInteger totsecs = idl*3600;
+        totsecs += minute*60;
+        totsecs += second;
+        NSTimeInterval secondsSinceUnixEpoch = [[NSDate date]timeIntervalSince1970];
+        totsecs += ((int)secondsSinceUnixEpoch % 1000) / 1000;
+        float mtime = totsecs / .864;
+        int umtcd = ((mtime - (fmod(mtime,1000))) / 1000);
+        int umtqd = (mtime - (fmod(mtime,1)) - (umtcd * 1000));
+        NSString *cd = (umtcd < 10) ? @"0" : @"";
+        NSString * qd1 = (umtqd < 10) ? @"0" : @"";
+        NSString * qd2 = (umtqd < 100) ? @"0" : @"";
+        NSString * decimalValue =  [NSString stringWithFormat:@"%@%i.%@%@%i UMT", cd, umtcd, qd1, qd2, umtqd];
+        return decimalValue;
+}
 
+void updateStatus(NSStatusItem *statusItem){
+    [statusItem setTitle:umt()];
+}
+
+
+
+@implementation AppDelegate
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
 
+
+-(void)updater{
+    [statusItem setTitle: umt()];
+}
+
+-(void)awakeFromNib{
+      statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem setMenu:statusMenu];
+    //updateStatus(statusItem);
+    [NSTimer scheduledTimerWithTimeInterval:2.0
+target:self
+selector:@selector(updater)
+userInfo:nil
+repeats:YES];
+    //[self updater];
+    [statusItem setHighlightMode:YES];
+}
 @end
